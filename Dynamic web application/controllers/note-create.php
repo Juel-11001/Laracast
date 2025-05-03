4b061560739ab499805d9076813311a153611994
Juel-11001 <mdjewel20172017@gmail.com>
2025-05-04 03:34:02 +0600

    form validation

--- Dynamic web application/controllers/note-create.php
+++ Dynamic web application/controllers/note-create.php
@@ -8,10 +8,20 @@ $db = new Database($config['mysql_database'], 'valet', '11001');
 // 	dd("you submited the form?");
 // }
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-	// dd($_POST);
-	$db->query('INSERT INTO posts(user_id, body) VALUES (:user_id, :body)', [
-		'user_id' => 1,
-		'body' => $_POST['note_body']]);
+	$errors = [];
+	if (strlen($_POST['note_body']) === 0) {
+		$errors['note_body'] = 'Body is required?';
+	}
+	if(strlen($_POST['note_body'])>100){
+		$errors['note_body']='The body can not be more than 100 char?';
+	}
 
+	if (empty($errors)) {
+		$db->query('INSERT INTO posts(user_id, body) VALUES (:user_id, :body)', [
+			'user_id' => 1,
+			'body' => $_POST['note_body']
+		]);
+	}
 }
+
 require "views/note-create.view.php";
--- Dynamic web application/views/note-create.view.php
+++ Dynamic web application/views/note-create.view.php
@@ -10,9 +10,11 @@
                         <div class="sm:col-span-3">
                             <label for="note_body" class="block text-sm/6 font-medium text-gray-900">Note Body</label>
                             <div class="mt-2">
-                                <textarea name="note_body" id="note_body" autocomplete="given-name"
-                                          class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
-                                </textarea>
+                                <textarea name="note_body" id="note_body" cols="60" rows="5"
+                                          placeholder="Here's an idea for your notes..."><?= $_POST['note_body'] ?? '' ?></textarea>
+								<?php if ($errors['note_body']) : ?>
+                                    <p class="mt-3 text-red-500 text-xs"><?= $errors['note_body'] ?></p>
+								<?php endif; ?>
                             </div>
                         </div>
                     </div>
